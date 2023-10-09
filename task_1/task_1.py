01959a1ad108bade537ce553ca69fa7bf9fbc55e
Viktoryia Naskevich <vika8nask@gmail.com>
2023-10-09 20:28:10 +0300

    added methods for the event class

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -1,40 +1,58 @@
+import json
 import random
-from datetime import datetime, date, time, timezone, timedelta
+from datetime import datetime, timezone, timedelta
 import string
 
 
-# a = datetime(2023, 5, 19, 21, 49,
-#              tzinfo=timezone(timedelta(hours=3)))
-# print(a)
-#
-# event_type_list = ['private', 'meeting', 'corporate', 'other']
-# print(random.choice(event_type_list))
-#
-#
-def event_place(length):
-    place_list = ['zoom', 'telegram']
-    letters = string.ascii_lowercase
-    rand_string = ''.join(random.choice(letters) for i in range(length))
-    print("Random string of length", length, "is:", rand_string or random.choice(place_list))
+class Event:
+
+    @staticmethod
+    def event_name():
+        letters_and_digits = string.ascii_letters + string.digits
+        name = ''.join(random.sample(letters_and_digits, 20))
+        event_name = ['event_name']
+        return event_name.append(name)
+
+    @staticmethod
+    def place():
+        place_list = ['zoom', 'telegram']
+        letters = string.ascii_lowercase
+        place = ''.join(random.choice(letters) for i in range(10))
+        lst_place = ['place']
+        return lst_place.append(place or random.choice(place_list))
+
+    @staticmethod
+    def event_type():
+        event_type_list = ['private', 'meeting', 'corporate', 'other']
+        event_type = random.choice(event_type_list)
+        lst_type = ['type']
+        return lst_type.append(event_type)
+
+    @staticmethod
+    def event_date():
+        year = random.randint(2000, 2023)
+        month = random.randint(1, 12)
+        day = random.randint(1, 31)
+        hour = random.randint(0, 23)
+        minute = random.randint(0, 59)
+        time_del = random.randint(-12, 14)
+
+        event_date = datetime(year, month, day, hour, minute,
+                              tzinfo=timezone(timedelta(hours=time_del)))
+        lst_date = ['date']
+        return lst_date.append(event_date)
+
+    def member(self):
+        member = ['Scott Pierce', 'James Gonzales', 'Ian Price', 'Tim Holmes', 'Joseph Coleman']
+        lst_members = ['members']
+        return lst_members.append(member)
+
+    def make_dict(self):
+        pass
 
 
-event_place(random.randint(0, 20))
-
-member_name = ['']
-
-# def event_name(length):
-#     letters_and_digits = string.ascii_letters + string.digits
-#     rand_string = ''.join(random.sample(letters_and_digits, length))
-#     print("Alphanum Random string of length", length, "is:", rand_string)
-#
-#
-# event_name(random.randint(0, 20))
+event_1 = Event()
 
 
-class Ivent:
-    def __init__(self, event_date, event_type, event_name, member, place):
-        self.event_date = event_date
-        self.event_type = event_type
-        self.event_name = event_name
-        self.member = member
-        self.place = place
+class Encoder(json.JSONEncoder):
+    pass
